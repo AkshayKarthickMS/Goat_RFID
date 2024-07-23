6bf7da3e8ace043a7debfdef6c454cfc0a13bfe2
AkshayKarthickMS <117292543+AkshayKarthickMS@users.noreply.github.com>
2024-07-23 17:22:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 import serial
+import serial.tools.list_ports
 import threading
 import time
 import base64
